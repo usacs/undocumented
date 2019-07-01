a57d81ed14717a2b7806ff5ef22fb7015449b49e
Sail338 <srihari.shankar1@gmail.com>
2019-07-01 19:16:05 -0400

    sampled daat

--- load_into_table.py
+++ load_into_table.py
@@ -3,11 +3,20 @@ from pandas.io import sql
 import sqlalchemy
 import os
 
-def load_into_sql():
-    engine = sqlalchemy.create_engine('sql_String')
-    for root, dirs, files in os.walk(".", topdown=False):
+def load_into_sql(path):
+    engine = sqlalchemy.create_engine('mysql://root:MPxYZQ8SxpbZ6y@immdb.cejces4w259w.us-east-1.rds.amazonaws.com:3306/data')
+    for root, dirs, files in os.walk(path, topdown=False):
          for name in files:
-            csv_to_read = f'{root}/{name}'
-            a = pd.read_csv(csv_to_read)
-            table_name = name[:-4]
-            a.to_sql(con=engine, name=table_name, if_exists='replace' )
+            if ".csv" in name:
+                csv_to_read = f'{root}/{name}'
+                print(csv_to_read)
+                try:
+                    a = pd.read_csv(csv_to_read,sep='\t', nrows = 3000)
+                    table_name = name[:-4]
+                    a.to_sql(con=engine, name=table_name, if_exists='replace' )
+                except:
+                    a = pd.read_csv(csv_to_read,sep='\t+', nrows = 3000)
+                    table_name = name[:-4]
+                    a.to_sql(con=engine, name=table_name, if_exists='replace' )
+
+load_into_sql("/Users/hari/downloads/gay")
