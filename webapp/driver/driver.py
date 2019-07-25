ed2fd6b10c15b133049dff91bd793291d9750c58
Sam Azouzi <sazouzi21@gmail.com>
2019-07-24 22:51:02 -0400

    finished driver, I hope, added some routes to views.py

--- webapp/driver/driver.py
+++ webapp/driver/driver.py
@@ -1,49 +1,77 @@
-import sql
+from sql import SQL
+from operator import itemgetter
 # class to write querie
 NUM_ENTRIES_PER_PAGE = 100
 
-def get_base_table(sql, curr_page = 0):
-    query = f'SELECT * FROM A_TblCase LIMIT {NUM_ENTRIES_PER_PAGE * (curr_page +1 )} OFFSET {curr_page  * NUM_ENTRIES_PER_PAGE}'
+def get_table(sql, curr_page = 0,tableName='A_TblCase'):
+    query = f'SELECT * FROM {tableName} LIMIT {NUM_ENTRIES_PER_PAGE * (curr_page +1 )} OFFSET {curr_page  * NUM_ENTRIES_PER_PAGE}'
     return (sql.SelectQuery(query,one = False))
 
 #To get a new set of tables we need the following:
 #1. The Query used to obtain the last set of tables
 #2. The new column name and value to be added to the new query
 #3. A list of tables used for the previous set of tables.
-def new_tables(sql,currQuery,prevTables,addition):
-    columnName = addition[columnName]
-    value = addition[value]
+def new_tables(sql,oldQuery,oldData,addition):
+    newData = {}
+    columnName = addition['columnName']
+    value = addition['value']
     tablesQuery = "SELECT TABLE_NAME FROM data.INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = \'BASE TABLE\'"
     tables = sql.SelectQuery(tablesQuery,one=False)
-    newQuery = currQuery + "AND "+columnName+"="+value
+    newQuery = oldQuery + "AND "+columnName+"="+value
+    newData["currentQuery"] = newQuery
 
     #Find the tables that have this column
     tableMatch = []
     for table in tables:
         columns = get_columns(sql,table)
-        numMatchingColumns = contains_query_columns(sql,currQuery,columns)
+        numMatchingColumns = contains_query_columns(sql,newQuery,columns)
         if numMatchingColumns != 0:
             insertTable(numMatchingColumns,table,columns,tableMatch)
 
-    response = []
+    newData["tables"] = []
     #Each entry of tableMatch is a tuple containing: (number of matching columns with new query, columns in current table, table Name)
     for table in tableMatch:
         newTable = {}
         newTable["tableName"] = table[2]
-        newTable["colMatch"] = table[0]
-        newTable["columns"] = table[1]
+        newTable["numberOfMatchingColumns"] = table[0]
+        newTable["columnNames"] = table[1]
+        newTable["tableContent"] = []
 
         #Get all the rows for this table
         query = "SELECT * FROM "+table[2]+" WhHERE "+newQuery
         rows = sql.SelectQuery(query,one=False)
-        #TODO
-        #1. Add each row to your newTable
-        #2. Add it to response
+        rowData = []
+        for row in rows:
+            for value in row.values():
+                value = value.replace(" ","")
+                rowData.append(value)
+            newTable["tableContent"].append(rowData)
+            rowData = []
+        newData["tables"].append(newTable)
 
-    return response
 
 
 
+    return newData
+
+def insertTable(numMatchingColumns,table,columns,tableMatch):
+    tableMatch.append( (table,numMatchingColumns,columns) )
+    tableMatch.sort(key=itemgetter(1))
+    return
+
+#Looking at the current query check to see if all the columns exist in the list of coclumns specified
+def contains_query_columns(sql,query,columns):
+    numMatching = 0
+    #Get the columns that we want:
+    # Query Looks like this: a=3 AND b=2 AND c=5
+    columnsList = query.replace(" ","").split("AND")
+    for i in range(0,len(columns)):
+        columns[i] = columns[i].toUpper()
+    for d in columnsList:
+        if d.split("=")[0].toUpper() in columns:
+            numMatching +=1
+
+    return numMatching
 
 
 def get_columns(sql,table):
